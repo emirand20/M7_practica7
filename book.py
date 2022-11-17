9ae6ad330689b231be7844e6201759631a19f4ee
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-11-17 20:46:42 +0100

    p2

---
+++ book.py
@@ -0,0 +1,44 @@
+import json
+def crearLibros():
+    books = """
+    {
+        "book":[
+            {"title":"Don quijote",
+            "cover":"Dura",
+            "year":"1615",
+            "pages":"462"
+            },
+            {"title":"Pinocho",
+            "cover":"Blanda",
+            "year":"1985",
+            "pages":"357"
+            },
+            {"title":"365 dni",
+            "cover":"Blanda",
+            "year":"2018",
+            "pages":"496"
+            },
+            {"title":"Cocina fácil y rico",
+            "cover":"Dura",
+            "year":"2022",
+            "pages":"704"
+            }
+        ]S
+    }
+    """
+    with open("books","w") as libros:
+        json.dump(books,libros)
+        print("Se ha creado el archivo")
+
+    with open("books", "r") as libros:
+         mustra = json.load(libros)
+         print(mustra)
+
+def muestraConsola():
+    with open("books", "r") as libros:
+        mustra = json.load(libros)
+        print(mustra)
+
+print(crearLibros())
+print("Funcion de mostrar")
+print(muestraConsola())
